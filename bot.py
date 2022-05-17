2c68a6d7a65e50c8ac532b6d732acc6c7fe2f00c
KingCio <103745710+cioyourfvboynih@users.noreply.github.com>
2022-05-17 17:30:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL1}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/pantekyks for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/whycio for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
             try:
@@ -42,26 +42,26 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/pantekyks for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/whycio for support")
         if FORCE_SUB_CHANNEL3:
             try:
                 link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
-                self.invitelink1 = link
+                self.invitelink3 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/pantekyks for support")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/whycio for support")
                 sys.exit()
        if FORCE_SUB_CHANNEL4:
             try:
                 link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL2)
-                self.invitelink2 = link
+                self.invitelink4 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/pantekyks for support")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL4 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/whycio for support")
                 sys.exit()       sys.exit()
         
         try:
@@ -72,11 +72,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/pantekyks for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/whycio for support")
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by shiro xynn\nhttps://t.me/shroxynn")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by kingCio cio\nhttps://t.me/cioyourfvboy")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
