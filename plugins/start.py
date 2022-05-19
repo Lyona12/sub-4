018fd95a1359eef0ce23f8e84bb2b2c584a182ec
KingCio <103745710+cioyourfvboynih@users.noreply.github.com>
2022-05-19 17:47:39 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -120,6 +120,9 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
             InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink4),
+        ],
+        [
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink5),
         ]
     ]
     try:
